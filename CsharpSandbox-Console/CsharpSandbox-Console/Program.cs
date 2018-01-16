7b8b6fe57634e6e4daf30477f310bffe55f4d0ce
Matt Detwiler <mattdetwiler@gmail.com>
2018-01-15 23:29:39 -0500

    Added in ability to control teleprompter speed via < and > also added TelePrompterConfig class

--- CsharpSandbox-Console/CsharpSandbox-Console/Program.cs
+++ CsharpSandbox-Console/CsharpSandbox-Console/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
 
+
 namespace CsharpSandbox_Console
 {
     class Program
@@ -13,6 +14,7 @@ namespace CsharpSandbox_Console
             var lines = ReadFrom("samplequotes.txt");
             foreach (var line in lines)
             {
+                RunTeleprompter().Wait();
                 Console.WriteLine(line);
                 if (!string.IsNullOrWhiteSpace(line))
                 {
@@ -35,15 +37,56 @@ namespace CsharpSandbox_Console
                     {
                         yield return word + " ";
                         lineLength += word.Length + 1;
-                        if (lineLength >70)
+                        if (lineLength > 70)
                         {
                             yield return Environment.NewLine;
                             lineLength = 0;
                         }
                     }
                     yield return Environment.NewLine;
-                }                     
+                }
             }
         }
+        private static async Task ShowTeleprompter(TeleprompterConfig config)
+        {
+            var words = ReadFrom("sampleQuotes.txt");
+            foreach (var line in words)
+            {
+                Console.Write(line);
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    await Task.Delay(config.DelayInMilliseconds);
+                }
+            }
+            config.SetDone();
+        }
+        private static async Task GetInput(TeleprompterConfig config)
+        {
+            Action work = () =>
+            {
+                do
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == '>')
+                    {
+                        config.UpdateDelay(-10);
+                    }
+                    else if (key.KeyChar == '<')
+                    { 
+                        config.UpdateDelay(10);
+                    }
+                } while (!config.Done);
+            };
+            await Task.Run(work);
+        }
+        private static async Task RunTeleprompter()
+        {
+            var config = new TeleprompterConfig();
+            var displayTask = ShowTeleprompter(config);
+
+            var speedTask = GetInput(config);
+            await Task.WhenAny(displayTask, speedTask);
+        }
     }
-}
+}    
+
---
+++ CsharpSandbox-Console/CsharpSandbox-Console/TelePrompterConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Math;
+
+namespace CsharpSandbox_Console
+{
+    internal class TeleprompterConfig
+    {
+        private object lockHandle = new object();
+        public int DelayInMilliseconds { get; private set; } = 200;
+        public bool Done => done;
+        private bool done;
+        public void SetDone()
+        {
+            done = true;
+        }
+
+        public void UpdateDelay(int increment)
+        {
+            var newDelay = Min(DelayInMilliseconds + increment, 1000);
+            newDelay = Max(newDelay, 20);
+            lock (lockHandle)
+            {
+                DelayInMilliseconds = newDelay;
+            }
+        }
+     }
+}
