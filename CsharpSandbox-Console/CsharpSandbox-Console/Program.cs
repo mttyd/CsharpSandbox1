e71e137ae09d648575b16f50fd8c639de3db58d7
Matt Detwiler <mattdetwiler@gmail.com>
2018-01-15 21:46:29 -0500

    Hello Matt console app

---
+++ CsharpSandbox-Console/CsharpSandbox-Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CsharpSandbox_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Matt";
+            Console.WriteLine("Hello "+name);
+            Console.Read();
+        }
+    }
+}
