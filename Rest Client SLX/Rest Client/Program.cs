4559860235c65a7503484e836a0f92bba38fe6cf
Matt Detwiler <mattdetwiler@gmail.com>
2018-01-19 21:39:25 -0500

    Initial sequence of cards and suits

---
+++ Rest Client SLX/Rest Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+namespace Rest_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repositories = ProcessRepositories().Result;
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine(repo.LastPush);
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            var client = new HttpClient();
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+            return repositories;
+
+  
+                
+        }
+    }
+}
